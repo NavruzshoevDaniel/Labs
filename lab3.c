2999ba5eb3376fb8b6ca6fd286037f88ac12ea13
NavruzshoevDaniel <den.navruzshoev@gmail.com>
2018-12-13 20:53:41 +0700

    Create lab3.c

---
+++ lab3.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void check(unsigned char word[], unsigned char str[], int ind, int a) {
+
+    for (int i = 0; i < strlen(word); i++) {
+        printf("%d ", ind + i - strlen(word) + 1);
+        if (word[i] != str[i + a - strlen(word)])
+            break;
+    }
+}
+
+long hashFunction(unsigned char word[], int count) {
+    long sum = 0;
+    int power[] = { 1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, 14348907,
+                    43046721 };
+    for (int i = 0; i < count; i++) {
+        sum += (word[i] % 3)*power[i];
+    }
+    return sum;
+}
+
+int main() {
+    unsigned char word[255] = { 0 };
+    gets(word);
+    long hashword = hashFunction(word, strlen(word));
+    int ind = 1;
+    long hashcount = 0;
+    int power[] = { 1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, 14348907,
+                    43046721 };
+    unsigned long k = 0, fchar = 0, len = 0;
+    int l = strlen(word);
+    printf("%ld ", hashword);
+    long step = power[ l - 1];
+    char buf[1000] = { 0 };
+    while (fgets(buf + len, 1000, stdin)) {
+        int i = 0, j = 0;
+        hashcount = 0;
+        while (i != (strlen(buf))) {
+            k = buf[i];
+
+            if (i + 1 == l) {
+                hashcount = hashFunction(buf, i + 1);
+
+            }
+            else if (i + 1 > l) {
+                hashcount = ((hashcount - fchar % 3) / 3) + (k % 3) * step;
+                j++;
+            }
+
+            if (hashcount == hashword) {
+                check(word, buf, ind, i + 1);
+            }
+            fchar = buf[j];
+            ind++;
+            i++;
+        }
+        memmove(buf, buf + (strlen(buf) - (l - 1)), 100 - l);
+    }
+
+
+
+    return 0;
+}
